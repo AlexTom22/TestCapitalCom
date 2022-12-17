dae670010dc44e6d2b7fffba6ce574dd6b8fdc3e
Alexander Tomelo <qa.tomelo.an@gmail.com>
2022-12-17 16:28:19 +0300

    add src/src/py

---
+++ src/src.py
@@ -0,0 +1,12 @@
+class CapitalComPageSrc:
+    URL = "https://capital.com/"
+
+
+class TradingViewPageSrc:
+    TITLE = "PRODUCTS"
+    LINK = ""
+    URL = "https://www.tradingview.com/broker/Capitalcom/"
+
+
+class ESGPageSrc:
+    URL = "https://capital.com/esg-rating"
