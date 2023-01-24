fb64ec0f71a9c640a64fdc2e15830bc717f52fe8
Alexander Tomelo <qa.tomelo.an@gmail.com>
2023-01-24 23:17:42 +0300

    restr file dir

---
+++ pages/capital/footer.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+# from .locators import FooterElementLocators
+
+
+class FooterElement(BasePage):
+    pass
+
+#
+#     # Opens hamburger menu
+#     def click_hamburger_menu(self):
+#         self.element_is_visible(SideBarLocator.HAMBURGER)
+#         self.browser.find_element(*SideBarLocator.HAMBURGER).click()
+#
+#     # Select and click LOGOUT item from hamburger menu
+#     def click_logout_from_top_left_menu(self):
+#         self.element_is_visible(SideBarLocator.LOGOUT)
+#         self.browser.find_element(*SideBarLocator.LOGOUT).click()
