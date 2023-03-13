221bb936821bb8443e3ea3e69a60f7086db150c5
LiudmilaDankevich <Dankevich_79@mail.ru>
2023-03-13 18:46:31 +0300

    test_03_02

--- pages/Learn/learn_to_trade_locators.py
+++ pages/Learn/learn_to_trade_locators.py
@@ -5,6 +5,7 @@ class LearnToTradePageLocator:
     LOCATOR_LEARN_TO_TRADE_TEXT = (By.CSS_SELECTOR, "div.wrap>nav>span")
     LOCATOR_LEARN_TO_TRADE_LOGIN_BUTTON = (By.ID,
                                            "wg_loginBtn")
-    LOCATOR_LEARN_TO_TRADE_TRADE_NOW_BUTTON = (By.CLASS_NAME,
-                                               "cc-header__signup btn btn--sm btn--darkText hideSm js_signup")
-    LOCATOR_LEARN_TO_TRADE_CREATE_VERIFY_YOUR_ACCOUNT_BUTTON = (By.CLASS_NAME, 'regSteps__item js_signup')
+    LOCATOR_LEARN_TO_TRADE_TRADE_NOW_BUTTON = (By.CSS_SELECTOR,
+                                               "div.cc-header__wrap > div#wphWrap a.cc-header__signup")
+    LOCATOR_LEARN_TO_TRADE_CREATE_VERIFY_YOUR_ACCOUNT_BUTTON = (By.CSS_SELECTOR,
+                                                                'div.regSteps__shape > i.regSteps__item.js_signup')
--- tests/Learn/us_03_Learn_to_trade/us_03_learn_to_trade_test.py
+++ tests/Learn/us_03_Learn_to_trade/us_03_learn_to_trade_test.py
@@ -75,3 +75,42 @@ class Test_US_03:
             page3.close_login_form()
         else:
             pytest.mark.skip(f"This test not for 'Auth' role")
+
+    @allure.feature("F_03_02 | Testing header")
+    @allure.story("S_01.02 | Testing 'Trade_Now' button on the header")
+    @allure.step("Start test button 'Trade_Now' on header.")
+    @allure.title("TC_03_02 with parameters: {cur_role}, {cur_language}, {cur_license}.   {datetime_now}")
+    def test_03_01_header_button_trade_now(
+            self, d, cur_login, cur_password, cur_language, cur_license, cur_role, prob_run_tc, datetime_now):
+        """
+        Check: Header -> button [Log In]
+        Language: En. License: FCA.
+        """
+        if prob_run_tc != "":
+            pytest.skip(f"{prob_run_tc}   {datetime_now}")
+
+        page3 = Conditions(d, "")
+        test_link = page3.preconditions(
+            d, CapitalComPageSrc.URL, "", cur_login, cur_password, cur_role, cur_language, cur_license
+        )
+
+        if cur_role == "NoReg":
+            page3 = Header(d, test_link)
+            if not page3.current_page_is(test_link):
+                page3.open_page()
+            page3 = BurgerMenu(d, test_link)
+            page3.burger_menu_click(d)
+            page3.menu_section_learn_to_trade_click(d, cur_language)
+            page3.click_learn_to_trade_item(d, cur_language)
+            page3 = LearnToTrade(d, test_link)
+            page3.tc_03_current_url()
+            page3.tc_03_should_be_learn_to_trade_text()
+
+            page3.tc_03_02_click_button_trade_now()
+
+            page3 = SignupLogin(d, test_link)
+            page3.should_be_signup_page(cur_language)
+            page3.close_signup_page()
+        else:
+            pytest.mark.skip(f"This test not for 'Auth' role")
+
